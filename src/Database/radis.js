1e318f5fd49ad0e66624229334b535db2b8deff5
Alex <137197995+Lexpalinya@users.noreply.github.com>
2024-08-05 22:15:27 +0700

    .

--- src/Database/radis.js
+++ src/Database/radis.js
@@ -1,10 +1,10 @@
 import { Redis } from "ioredis";
 import { REDIS_HOST, REDIS_PORT } from "../config/api.config.js";
 const redis = new Redis({
-  host:REDIS_HOST || 'redis',
+  host: REDIS_HOST || "localhost",
   port: REDIS_PORT || 6379,
   retryStrategy: (times) => {
-    const delay = Math.min(times * 50, 10000); // Retry with an exponential backoff
+    const delay = Math.min(times * 50, 15000); // Retry with an exponential backoff
     return delay;
   },
 });
