4158c348ce986df2892fcfceebd4fbbb9fc04357
Alex <137197995+Lexpalinya@users.noreply.github.com>
2024-08-05 22:32:45 +0700

    .

--- src/Database/radis.js
+++ src/Database/radis.js
@@ -1,11 +1,13 @@
-import { Redis } from "ioredis";
-import { REDIS_HOST, REDIS_PORT } from "../config/api.config.js";
+import Redis from 'ioredis';
+import { REDIS_HOST, REDIS_PORT } from '../config/api.config.js'; // Adjust path as needed
+
 const redis = new Redis({
-  host: REDIS_HOST || "localhost",
+  host: REDIS_HOST || 'redis', // Use the service name from docker-compose
   port: REDIS_PORT || 6379,
   retryStrategy: (times) => {
     const delay = Math.min(times * 50, 15000); // Retry with an exponential backoff
     return delay;
   },
 });
+
 export default redis;
