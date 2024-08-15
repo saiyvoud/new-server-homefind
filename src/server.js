c06bf920033372a9ed60f3abfc56a65baf952633
Alex <137197995+Lexpalinya@users.noreply.github.com>
2024-08-15 11:17:41 +0700

    cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
 import fileUpload from "express-fileupload";
 import { EAPI, SERVER_PORT } from "./config/api.config.js";
 import prisma from "../src/util/prismaClient.js";
@@ -20,16 +21,28 @@ app.use(fileUpload());
 app.use(morgan("dev"));
 
 // CORS Headers
-app.all("/*", (req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Accept, Access-Token, X-Requested-With"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
+// app.all("/*", (req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Content-Type, Accept, Access-Token, X-Requested-With"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//   next();
+// });
 
+app.use(
+  cors({
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Access-Token",
+      "Accept",
+    ],
+  })
+);
 // API Router
 app.use(EAPI, APIRouter);
 
@@ -40,21 +53,21 @@ const checkDatabaseConnection = async () => {
     console.log("Prisma Connected to the DATABASE Success: OK");
   } catch (err) {
     console.error("Error Connecting to DATABASE Fail:", err);
+    setTimeout(async () => {
+      console.log("Reconnecting to Database....");
+      await checkDatabaseConnection();
+    }, 60000);
   }
 };
 
-
-
 // await client.del("wallets");
-
 
 // const user = await redis.get("users");
 // console.log('user :>> ', user);
 // Start Server
 
-app.listen(SERVER_PORT, async() => {
+app.listen(SERVER_PORT, async () => {
   console.log(`Server is listening on http://localhost:${SERVER_PORT}`);
   console.log(`Server Already: ${SERVER_PORT}`);
   checkDatabaseConnection();
- 
 });
